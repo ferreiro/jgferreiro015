5f05f42fefeb35196c2a5bfc7e8e28954e46102d
Jorge Ferreiro <me@jgferreiro.com>
2015-09-17 03:11:21 +0200

    New files

---
+++ assets/js/scripts.js
@@ -0,0 +1,33 @@
+var openExplanation = $('#goToExplanation');
+var closeExplanation = $('#closeExplanation');
+var boxExplanation = $('#skillsExplanation');
+var openGeekMessage1 = $('#goToGeek');
+var selectOptionBox = $('#selectOptionBox');
+var goToProjects = $('#goProjects');
+
+goToProjects.click(function() {
+	// scrollToProjects
+	$('html, body').animate({ 
+		scrollTop: $('#projects').offset().top - 150
+	}, 'slow');
+	return false;
+})
+
+openExplanation.click(function() {
+	selectOptionBox.hide('500');
+	boxExplanation.delay('100').fadeIn('medium');
+	return false; // Prevent default
+});
+
+closeExplanation.click(function(e) {
+	var e = $(this);
+	console.log(e)
+	boxExplanation.hide(0);
+
+	return false; // Prevent default
+});
+
+openGeekMessage1.click(function() {
+	alert("Tioooo, eres super geek ehh");
+	return false;
+});
