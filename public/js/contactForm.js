2d04d497082d12e082fbd80eb6177aa2938fcfb6
Jorge Ferreiro <me@jgferreiro.com>
2015-10-12 18:46:30 +0200

    Checking if name and email are valid fields

--- public/js/contactForm.js
+++ public/js/contactForm.js
@@ -1,61 +1,88 @@
-$('form').submit(function(event){
-    var name, email, date, time, plan; // input userData.
+$('.open-contactForm').click(function() {
+    $('html, body').animate({ 
+        scrollTop: $('#contact').offset().top
+    }, 'slow');
+});
+
+$('#contactForm').submit(function(event){
+    var data, email, name;
     event.preventDefault()
-    
-    name = $('input[name="name"]').val();
+  
+    $('#formLoader').fadeIn(500); // show loader
+    $('#messageFailure').fadeOut("slow") // hide message fail
+
+    name = $('input[name="name"]');
+    email = $('input[name="email"]');
 
     data = {
-        "name" : name,
-        "email" : $('input[name="email"]').val(),
+        "name" : name.val(),
+        "email" : email.val(),
         "day" : $('select[name="day"]').val(),
         "month" : $('select[name="month"]').val(),
         "year" : $('select[name="year"]').val(),
         "time" : $('select[name="time"]').val(),
         "plan" : $('select[name="plan"]').val()
     }
+    
+    validName = isValidField(data.name, 0);
+    validEmail = isValidEmail(data.email);
 
-    $('#formLoader').fadeIn(500);
-    $('#messageFailure').fadeOut("slow")
+    if (validName && validEmail) {
 
-    $.ajax({
-        type        : 'post',       // define the type of HTTP verb we want to use (POST for our form)
-        url         : '/mail', // the url where we want to POST
-        dataType    : 'json',       // what type of userData do we expect back from the server
-        data        : data,     // our userData object
-        encode      : true
-    })
-    .done(function(returnObject) {
-        $('#contactForm').fadeOut(500);
-        $('#userName').html(data.name)
-        $('#userMail').html(data.email)
+        $.ajax({
+            type        : 'post',       // define the type of HTTP verb we want to use (POST for our form)
+            url         : '/mail', // the url where we want to POST
+            dataType    : 'json',       // what type of userData do we expect back from the server
+            data        : data,     // our userData object
+            encode      : true
+        })
+        .done(function(returnObject) {
 
-        if (returnObject.data.error) {
+            $('#contactForm').fadeOut(500);
+            $('#userName').html(data.name)
+            $('#userMail').html(data.email)
+
+            if (returnObject.data.error) {
+                $('#messageFailure').delay(500).fadeIn("slow") 
+                $('#contactForm').show(0);  // show the form again
+            }
+            else {
+                $('#messageSuccess').delay(500).show(0) 
+            }
+        })
+        .fail(function(returnObject) {
+            alert('Internal error on our server')
             $('#messageFailure').delay(500).fadeIn("slow") 
             $('#contactForm').show(0);  // show the form again
+        })
+        .always(function(returnObject) {
+            $('#formLoader').hide(500);
+        });
+
+    }
+    else {
+        errorClass = "contactFieldset-input-error";
+        
+        name.removeClass(errorClass);
+        email.removeClass(errorClass);
+
+        if (!validName) {
+            name.addClass(errorClass); 
         }
-        else {
-            $('#messageSuccess').delay(500).show(0) 
-            // $('#initialContactLayer').delay(500).fadeIn("slow")  
+        if (!validEmail) {
+            email.addClass(errorClass); 
         }
-    })
-    .fail(function(returnObject) {
-        alert('Internal error on our server')
-        $('#messageFailure').delay(500).fadeIn("slow") 
-        $('#contactForm').show(0);  // show the form again
-    })
-    .always(function(returnObject) {
-        $('#formLoader').hide(500);
-    });
+    }
+    
 
     return false; 
-
 }); 
 
 // Email validation function
 // Using regular expression to check if an email is valid or not
 // Source code: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript?page=1&tab=votes#tab-top
 
-function validateEmail(email) {
+function isValidEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
 }
